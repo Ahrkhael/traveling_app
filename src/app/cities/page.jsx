2efe66157c77a5074355f7f3c69d2e40aff13ce6
Ahrkhael <jjkako@gmail.com>
2024-10-05 15:34:51 +0200

    Refactor component CityList and CSS updated with mobile responsive

--- src/app/cities/page.jsx
+++ src/app/cities/page.jsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
+import CityList from "@/components/cityList/CityList";
 import citiesData from "../../../data/data.json";
 import styles from "./page.module.css";
 
@@ -9,31 +8,14 @@ const CitiesPage = () => {
   return (
     <main className="main">
       <h1 className="title">Cities page</h1>
-      <ul className={styles.list}>
-        {cities.map((city) => (
-          <li key={city.city}>
-            <Link href={`/${city.city}`}>
-              <Image
-                src={city.image}
-                width={200}
-                height={200}
-                alt={`Foto de la ciudad de ${city.city}`}
-                className={styles.img}
-              />
-              <h2 className="title">{city.city}</h2>
-              <p className="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </Link>{" "}
-          </li>
-        ))}
-      </ul>
+      <CityList
+        cities={cities}
+        listStyles={styles.list}
+        listItemStyles={styles.listItem}
+        imgStyles={styles.img}
+        titleStyles={"title"}
+        descriptionStyles={"description"}
+      />
     </main>
   );
 };
